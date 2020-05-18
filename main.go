f3a1dda1f86c035f0e2e3c263d76097e4a3fb41e
Phil Turner <julietius@gmail.com>
2020-05-18 17:04:30 +0100

    update logger reference

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ func main() {
 	port := 8080
 	logger := log.New(os.Stdout, "[SI-Convert] ", log.LstdFlags|log.Lshortfile)
 	a := api.New(logger)
-	log.Printf("Api is listening on port: %v", port)
+	logger.Printf("Api is listening on port: %v", port)
 	if err := a.Start(port); err != http.ErrServerClosed {
-		log.Fatalf("Api has closed unexpectly: %v", err)
+		logger.Fatalf("Api has closed unexpectly: %v", err)
 	}
-	log.Print("Exiting Api service")
+	logger.Print("Exiting Api service")
 }
